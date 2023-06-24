031d28c2f88ba5cdfcf12a57248a2920d48736d8
Said Gadjiev <ackusha1984@ya.ru>
2023-06-24 17:35:09 +0300

    add configuretion Huawei1

--- doWorking.py
+++ doWorking.py
@@ -1,10 +1,12 @@
 # #
-# def commHuawei(self):
-#     huawei_router = {
-#         'device_type': 'huawei',
-#         'host': '10.30.111.10',
-#         'username': 'admin',
-#         'password': 'fufvtvyjy',
-#         'secret': 'enablepass',
-#         # 'port': 20,
-#     }
+def commHuawei():
+    huawei_router = {
+        'device_type': 'huawei',
+        'host': '10.30.111.10',
+        'username': 'admin',
+        'password': 'fufvtvyjy',
+        'secret': 'enablepass',
+        # 'port': 20,
+    }
+    print("fvdfgfdg")
+    return "dfgdfgdfgf"
--- main.py
+++ main.py
@@ -1,14 +1,40 @@
+import sys
+
+import disein
+
 import netmiko
 
-huawei_router = {
-    'device_type': 'huawei',
-    'host': '10.30.111.10',
-    'username': 'admin',
-    'password': 'fufvtvyjy',
-    'secret': 'enablepass',
-    # 'port': 20,
-}
+from PyQt5 import QtWidgets
 
-ssh = netmiko.ConnectHandler(**huawei_router)
-result = ssh.send_command('dis int br')
-print(result)
+
+class MyWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        # Вызываем метод для загрузки интерфейса из класса Ui_MainWindow,
+        self.ui = disein.Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.connectBTN.clicked.connect(self.comm_huawei)
+        # self.ui.label_3.setText(self.btnTest())
+
+    # Коммутатор Huawei
+    def comm_huawei(self):
+        huawei_router = {
+            'device_type': 'huawei',
+            'host': '10.30.111.10',
+            'username': 'admin',
+            'password': 'fufvtvyjy',
+            'secret': 'enablepass',
+            # 'port': 20,
+        }
+
+        ssh = netmiko.ConnectHandler(**huawei_router)
+        result = ssh.send_command('dis int br')
+        self.ui.label_3.setText(result)
+        print(result)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    sys.exit(app.exec_())

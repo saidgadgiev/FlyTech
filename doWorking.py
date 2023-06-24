3bdd028e4b55d57b1d6166cb997cb51dc62c92ed
Said Gadjiev <ackusha1984@ya.ru>
2023-06-24 15:40:46 +0300

    add configuretion Huawei

---
+++ doWorking.py
@@ -0,0 +1,10 @@
+# #
+# def commHuawei(self):
+#     huawei_router = {
+#         'device_type': 'huawei',
+#         'host': '10.30.111.10',
+#         'username': 'admin',
+#         'password': 'fufvtvyjy',
+#         'secret': 'enablepass',
+#         # 'port': 20,
+#     }
--- testmain.py
+++ testmain.py
@@ -1,12 +1,40 @@
 import sys
 
-from disein import Ui_MainWindow
+import disein
+
+import netmiko
+
 from PyQt5 import QtWidgets
+
+
+class MyWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        # Вызываем метод для загрузки интерфейса из класса Ui_MainWindow,
+        self.ui = disein.Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.connectBTN.clicked.connect(self.comm_huawei)
+        # self.ui.label_3.setText(self.btnTest())
+
+    # Коммутатор Huawei
+    def comm_huawei(self):
+        huawei_router = {
+            'device_type': 'huawei',
+            'host': '10.30.111.10',
+            'username': 'admin',
+            'password': 'fufvtvyjy',
+            'secret': 'enablepass',
+            # 'port': 20,
+        }
+
+        ssh = netmiko.ConnectHandler(**huawei_router)
+        result = ssh.send_command('dis int br')
+        self.ui.label_3.setText(result)
+        print(result)
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    MaWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MaWindow)
-    MaWindow.show()
+    window = MyWindow()
+    window.show()
     sys.exit(app.exec_())

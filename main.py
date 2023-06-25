c33aa0579d855ccee44172c1bc6dbe807d6785dd
Said Gadjiev <ackusha1984@ya.ru>
2023-06-25 13:21:31 +0300

    add list mac

--- main.py
+++ main.py
@@ -13,16 +13,29 @@ class MyWindow(QtWidgets.QMainWindow):
         # Вызываем метод для загрузки интерфейса из класса Ui_MainWindow,
         self.ui = disein.Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.interfBTN.clicked.connect(self.comm_huawei_f)
+
+        self.ui.interfBTN.clicked.connect(self.comm_huawei_interf)  # кнопка просмотр портов
+        self.ui.listMacBTN.clicked.connect(self.comm_huawei_listMac)  # Кнопка список маков
+        # self.ui.serchMacBTN.clicked.connect()  # кнопка поиск по маку
+
         self.ip_address = self.ui.ipAddressEdit.text
 
         # self.ui.label_3.setText(self.btnTest())
 
-    # Коммутатор Huawei
-    def comm_huawei_f(self):
+    # Коммутатор Huawei просмотр интерфейсов
+    def comm_huawei_interf(self):
         ssh = doWorking.comm_huawei(self.ip_address)
         ssh = netmiko.ConnectHandler(**ssh)
         result = ssh.send_command('dis int br')
+        # print("ssh1")
+        self.ui.resultEdit.setText(result)
+
+    # Коммутатор Huawei просмотр маков
+    def comm_huawei_listMac(self):
+        ssh = doWorking.comm_huawei(self.ip_address)
+        ssh = netmiko.ConnectHandler(**ssh)
+        result = ssh.send_command('display mac-address')
+        # print(result)
         self.ui.resultEdit.setText(result)
 
 

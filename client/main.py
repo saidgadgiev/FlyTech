1778984e6990ce6e9464265d4c4ae9131f35b3d9
Said Gadjiev <ackusha_dargo@bk.ru>
2024-05-02 14:21:30 +0300

    Add proect klient

---
+++ client/main.py
@@ -0,0 +1,52 @@
+import socket
+import sys
+from PyQt5.QtWidgets import (QLineEdit, QInputDialog)
+from PyQt5 import QtWidgets
+import netmiko
+import gui
+
+class MyWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        # Вызываем метод для загрузки интерфейса из класса Ui_MainWindow
+        self.ui = gui.Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+        self.ip_address = self.ui.ipAddressEdit.text # IP адрес из строки ввода
+        self.port_switchboard = self.ui.portEdit.text # порт коммутатора к кот подключен абонент
+        self.interfaceClient = self.ui.interficeEdit.text # интерфейс абонента
+        
+        self.ui.start_check_GPON.clicked.connect(self.infoClientGpon) # Вывод информации
+        
+    def infoClientGpon(self):
+        try:
+            device = {
+            'device_type': 'zte_zxros_telnet',
+            'host': self.ip_address(),
+            'username': 'admin',
+            'password': 'fufvtvyjy',
+            }
+
+            connection = netmiko.ConnectHandler(**device)
+            command_info = connection.send_command(f'show pon onu information gpon-onu_1/{self.port_switchboard()}:{self.interfaceClient()}')
+            command_signal_strength = connection.send_command(f'show pon power attenuation gpon-onu_1/{self.port_switchboard()}:{self.interfaceClient()}')
+            command_show_port = connection.send_command(f'show gpon remote-onu interface eth gpon-onu_1/{self.port_switchboard()}:{self.interfaceClient()}')
+            command_show_macc = connection.send_command(f'show mac gpon onu gpon-onu_1/{self.port_switchboard()}:{self.interfaceClient()}')
+            command_show_config = connection.send_command(f'show running-config interface gpon-olt_1/{self.port_switchboard()}')
+            
+            result = '\nИНФОРМАЦИЯ ОБ ОНУШКЕ\n' + command_info +'\n\n УРОВЕНЬ СИГНАЛА \n\n' +  command_signal_strength + '\n\nПРОСМОТР МАКОВ НА ОНУ\n\n' + command_show_macc + '\n\nПРОСМОТР ЛИНКОВ НА ПОРТУ ОНУШКИ\n\n' + command_show_port
+            connection.disconnect
+            
+            self.ui.resultEdit.setText(result) 
+            self.ui.result2Edit.setText(command_show_config)
+        except ValueError:
+            self.ui.resultEdit.setText('ВВЕДИТЕ IP ADDRESS УСТРОЙСТВА') 
+            self.ui.result2Edit.setText('ВВЕДИТЕ IP ADDRESS УСТРОЙСТВА')
+        except socket.gaierror:
+            self.ui.resultEdit.setText("Не удалось установить TCP-соединение с устройством. \n Проверьте правильно ли вы ввели IP address устройства ")
+            self.ui.result2Edit.setText("Не удалось установить TCP-соединение с устройством. \n Проверьте правильно ли вы ввели IP address устройства ")
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    sys.exit(app.exec_())
--- testmain.py
+++ testmain.py
@@ -1,42 +1,21 @@
 import socket
-
 import netmiko
 
-ip_address = input('Введите IP адресс -> ')
-login = input('Введите логин -> ')
-password = input('Введите пароль -> ')
+
 device = {
         'device_type': 'zte_zxros_telnet',
-        'host': ip_address,
-        'username': login,
-        'password': password,
+        'host': '158',
+        'username': 'admin',
+        'password': 'fufvtvyjy',
     }
-# device = switchboard_command.connectDlink(ip_address, login, password)
-ssh = None
-bol = True
-print("1. Подключить коммутатор")
-print("2. Просмотр интерфейсов")
-print("3. отключение")
-while bol == True:
-    comm = input('enter -> ')
-    if comm == "1":
-        try:
-            print('идет подключение')
-            ssh = netmiko.ConnectHandler(**device)
-            print("успех подключения")
-        except socket.gaierror:
-            print("Не удалось установить TCP-соединение с устройством. \nРаспространенными причинами этой проблемы "
-                "являются:\n1. Неверное имя хоста или IP-адрес.\n2. Неправильный TCP-порт.\n3. Промежуточный "
-                "брандмауэр, блокирующий доступ.\n")
-    elif comm == "2":
-        res = ssh.send_command("show ports")
-        print(res)
-    elif comm == "3":
-        ssh.disconnect()
-        print(res)
-    else:
-        print("false")
-        bol = False
-print(device)
-
-
+try:
+    connection = netmiko.ConnectHandler(**device)
+    result = connection.send_command('show gpon onu state gpon-olt_1/2/2')
+    connection.disconnect
+    print(result)
+except ValueError:
+    print('Enter ip address')
+except socket.gaierror:
+    print("Не удалось установить TCP-соединение с устройством. \nРаспространенными причинами этой проблемы "
+            "являются:\n1. Неверное имя хоста или IP-адрес.\n2. Неправильный TCP-порт.\n3. Промежуточный "
+            "брандмауэр, блокирующий доступ.\n")
